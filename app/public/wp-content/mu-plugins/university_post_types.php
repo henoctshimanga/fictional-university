3449cdee50bf787cf4c7453cd55f25b4b51d4f10
henoctshimanga <henoctshimanga79@gmail.com>
2025-02-20 21:31:09 -0500

    added a Program post type

--- app/public/wp-content/mu-plugins/university_post_types.php
+++ app/public/wp-content/mu-plugins/university_post_types.php
@@ -1,6 +1,8 @@
 <?php 
 
 function university_post_types() {
+    
+    // Event Post Type
     register_post_type('event', array(
         'rewrite' => array('slug' => 'events'),
         'supports' => array('title', 'editor', 'excerpt'),
@@ -15,6 +17,25 @@ function university_post_types() {
             'singular_name' => 'Event'
         ),
         'menu_icon' => 'dashicons-calendar'
+
+    ));
+
+
+    // Program Post Type
+    register_post_type('program', array(
+        'rewrite' => array('slug' => 'programs'),
+        'supports' => array('title', 'editor'),
+        'has_archive' => true,
+        'public' => true,
+        'show_in_rest' => true,
+        'labels' => array(
+            'name' => 'Programs',
+            'add_new_item' => 'Add New Program',
+            'edit_item' => 'Edit Program',
+            'all_items' => 'All Programs',
+            'singular_name' => 'Program'
+        ),
+        'menu_icon' => 'dashicons-awards'
 
     ));
 }
